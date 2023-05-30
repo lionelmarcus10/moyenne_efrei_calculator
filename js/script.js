94a55d8520d051f7988b21e6fed63d82e774287b
Bereket TK <bekitady@gmail.com>
2023-05-30 23:20:17 +0200

    code js pour les tableaux

---
+++ js/script.js
@@ -0,0 +1,340 @@
+let Efrei_Modules = {
+  "semestre 4": {
+    "Formation générale": {
+      "Dissertation et Rhétorique": {
+        "CE": 0.2,
+        "DE": 0.5,
+        "Projet": 0.3
+      },
+      "Economie": {
+        "DE": 0.7,
+        "NOTE": 0.3
+      },
+      "English 4 - Preparation for the Study Abroad Program": {
+        "CE": 0.2,
+        "CO": 0.2,
+        "DE": 0.4,
+        "TD20": 0.2
+      },
+      "Learning XP": {
+        "NoTE": 1
+      }
+    },
+    "UE - Informatique générale": {
+      "Introduction aux Bases de Données": {
+        "DE": 0.6,
+        "TD": 0.2,
+        "TP": 0.2
+      },
+      "Programmation Oriente Objet en java": {
+        "DE": 0.6,
+        "TD": 0.2,
+        "TP": 0.2
+      },
+      "Programmation Web": {
+        "DE": 0.5,
+        "PRJ": 0.3,
+        "TP": 0.2
+      }
+    },
+    "UE - Mathématiques": {
+      "Analyse de données": {
+        "CE": 0.4,
+        "DE": 0.6
+      },
+      "Automates finis et expressions rationnelles": {
+        "DE": 0.4,
+        "PRJ": 0.4,
+        "TD": 0.2
+      },
+      "Modélisation mathématique": {
+        "CTP": 0.2,
+        "DE": 0.6,
+        "PRJ": 0.2
+      }
+    },
+    "UE - Physique et électronique": {
+      "Propagation électromgnétique": {
+        "DE": 0.6,
+        "TAI": 0.1,
+        "TD": 0.3
+      },
+      "Systèmes de transmission": {
+        "CTD": 0.2,
+        "DE": 0.6,
+        "PRJ": 0.2,
+        "TP": 0
+      },
+      "Thermodynamique": {
+        "DE": 0.6,
+        "TAI": 0.1,
+        "TD": 0.3
+      }
+    },
+    "UE -Projet Transverse": {
+      "Projet transverse L2": {
+        "PRJ": 1
+      }
+    },
+    "UE -Stage L2": {
+      "Stage commercial": {
+        "": 2
+      }
+    }
+  },
+  "semestre 3": {
+    "UE -Formation générale": {
+      "Dissertation et critique d'un essai": {
+        "CE": 0.2,
+        "DE": 0.5,
+        "PRJ": 0.3
+      },
+      "Englhish 3 - Scientific and Technical English": {
+        "CE": 0.2,
+        "CO": 0.2,
+        "DE": 0.4,
+        "TD20": 0.2
+      },
+      "Introduction au Delivery de projet": {
+        "TP": 1
+      },
+      "Learnig XP": {
+        "NOTE": 1
+      },
+      "Révolution numérique et entrepreneuriat": {
+        "NOTE": 1
+      }
+    },
+    "UE -Informatique générale": {
+      "Algorithmique et structure de données 2": {
+        "DE": 0.5,
+        "PRJ": 0.15,
+        "TD": 0.2,
+        "TP": 0.15
+      },
+      "Architecture réseaux": {
+        "DE": 0.6,
+        "TD": 0.4,
+        "TP": 0
+      },
+      "Introduction au système Linux": {
+        "CTP": 0.4,
+        "DE": 0.6
+      }
+    },
+    "UE - Mathématiques": {
+      "Fonctions de plusieurs variables": {
+        "CTD": 0.4,
+        "DE": 0.6
+      },
+      "Introduction à Matlab": {
+        "DE": 0.6,
+        "TP": 0.4
+      },
+      "Probabilités": {
+        "CE": 0.4,
+        "DE": 0.6
+      }
+    },
+    "UE - Physique et électronique": {
+      "Champs électromagnétiques": {
+        "DE": 0.6,
+        "TD": 0.4
+      },
+      "Physique moderne": {
+        "DE": 0.6,
+        "TD": 0.4
+      },
+      "Systèmes numériques": {
+        "CTD": 0.1,
+        "DE": 0.6,
+        "PRJ": 0.1,
+        "TP": 0.2
+      }
+    }
+  },
+  "semestre 2": {
+    "UE - Formation générale": {
+      "English 2 - Issues in the English Speaking World": {
+        "CE": 0.2,
+        "CO": 0.2,
+        "DE": 0.4,
+        "TD20": 0.2
+      },
+      "Essai et écriture créative": {
+        "CE": 0.2,
+        "DE": 0.4,
+        "PRJ": 0.4
+      },
+      "Histoire des Sciences": {
+        "CE": 1
+      },
+      "Participation à la vie de l'école": {
+        "NOTE": 1
+      }
+    },
+    "UE - Informatique": {
+      "Algorithmique et structures de données 1": {
+        "CE": 0.2,
+        "DE": 0.4,
+        "PRJ": 0.15,
+        "TD": 0.1,
+        "TP": 0.15
+      },
+      "Introduction à GIT": {
+        "PRJ": 0.5,
+        "TP": 0.5
+      }
+    },
+    "UE - Physique et électronique": {
+      "De l'atome à la puce": {
+        "DE": 0.6,
+        "TAI": 0.1,
+        "TD": 0.3
+      },
+      "Du système à la fonction": {
+        "CTD": 0.1,
+        "DE": 0.6,
+        "PRJ": 0.1,
+        "TP": 0.2
+      },
+      "Electricité générale": {
+        "DE": 0.6,
+        "TD": 0.2,
+        "TP": 0.2
+      }
+    },
+    "UE - Projet Transverse L1": {
+      "Projet transverse": {
+        "PRJ": 1
+      }
+    },
+    "UE - Stage L1": {
+      "Stage d'exécution": {
+        "NOTE": 1
+      }
+    }
+  },
+  "semestre 1": {
+    "UE - Formation générale": {
+      "Analyse argumentative et communication orale": {
+        "CE": 0.2,
+        "DE": 0.5,
+        "TD20": 0.3
+      },
+      "English 1 - Technology and Society": {
+        "CE": 0.2,
+        "CO": 0.2,
+        "DE": 0.4,
+        "TD20": 0.2
+      },
+      "Participation à la vie de l'école": {
+        "NOTE": 1
+      }
+    },
+    "UE - Informatique": {
+      "Algorithmique": {
+        "CE": 0.3,
+        "DE": 0.5,
+        "TD": 0.2
+      },
+      "Programmation en Python": {
+        "CE": 0.3,
+        "DE": 0.3,
+        "PRJ": 0.2,
+        "TP": 0.2
+      }
+    },
+    "UE - Mathématiques": {
+      "Algèbre générale": {
+        "CE": 0.2,
+        "CTD": 0.4,
+        "DE": 0.4
+      },
+      "Analyse 1": {
+        "CE": 0.2,
+        "DE": 0.4,
+        "TD": 0.4
+      }
+    },
+    "UE - Physique et électronique": {
+      "Information numérique": {
+        "CTD": 0.2,
+        "DE": 0.5,
+        "TP": 0.3
+      },
+      "Matière et ondes": {
+        "DE": 0.6,
+        "TD": 0.4
+      },
+      "Mécanique et électricité": {
+        "DE": 0.6,
+        "TD": 0.4
+      }
+    }
+  }
+};
+
+
+function getSemesterByNumber(number){
+  return  Efrei_Modules[`semestre ${number}`];
+}
+
+function getSemesterUeNames(Semester_Number){
+  let UE = Object.keys(getSemesterByNumber(Semester_Number));
+  
+  return UE;
+}
+
+
+function getSemesterUeModuleNames(Semester_Number, UE_Name){
+  let Modules = Object.keys(Efrei_Modules[`semestre ${Semester_Number}`][UE_Name]);
+  
+  return Modules;
+}
+
+function getSemesterUeModuleStepsAndQuota(Semester_Number, UE_Name, Module_Name){
+  let Module = Efrei_Modules[`semestre ${Semester_Number}`][UE_Name][Module_Name];
+  console.log(Module)
+  // extraire les étapes ( de ce ctd... ) et leur coefficient dans un tableau double [[étape, coeff], [étape, coeff]...]]  avec Object.entries()
+  return Module;
+}
+
+
+
+
+// Fonction pour afficher le tableau selon le semestre sélectionné
+function afficherTableau(semestre) {
+  let semestreData = getSemesterByNumber(semestre);
+  let UE = getSemesterUeNames(semestre);
+  // Récupérer l'élément du tableau par son ID
+  var table = document.getElementById("myTable");
+
+  // Effacer le contenu du tableau
+  table.innerHTML = "";
+
+  // Créer la première ligne du tableau (en-tête)
+  var headerRow = document.createElement("tr");
+
+  var matiereHeader = document.createElement("th");
+  matiereHeader.innerHTML = "Matière";
+  headerRow.appendChild(matiereHeader);
+
+  table.appendChild(headerRow);
+ 
+  // Parcourir les matières du semestre et ajouter les lignes correspondantes au tableau
+  for (let i =0;  i < UE.length ; i++) {
+   
+    var matiereRow = document.createElement("tr");
+
+    var nomMatiere = document.createElement("td");
+    nomMatiere.innerHTML = UE[i];
+
+    matiereRow.appendChild(nomMatiere);
+
+    table.appendChild(matiereRow);
+    
+  }
+  
+}
+
